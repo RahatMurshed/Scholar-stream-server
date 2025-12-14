a8de615b745b476dc7b4e1f95767009603272d6a
Rahat Murshed <rahatmurshedtapader@gmail.com>
2025-12-14 11:29:41 +0600

    top scholarships and all scholarships api added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000;
 
 
@@ -37,7 +37,7 @@ async function run() {
     await client.connect();
 
 
-    const database = client.db("scholarStream");
+    const database = client.db("scholar-stream");
     const usersCollection = database.collection("users");
     const scholarshipsCollection = database.collection("scholarships");
     const applicationsCollection = database.collection("applications");
@@ -46,12 +46,67 @@ async function run() {
 
 
 
+    // Users Related API
+
     app.get('/users', async (req, res) => {
 
       const cursor = usersCollection.find();
       const users = await cursor.toArray();
       res.send(users);
 
+    })
+
+
+    // Scholarships Related API
+    app.get('/scholarships', async (req, res) => {
+
+      const cursor = scholarshipsCollection.find().project({ 
+        _id: 1,
+        scholarshipName: 1,
+        universityName: 1,
+        universityImage: 1,
+        universityCountry: 1,
+        universityCity: 1,
+        subjectCategory: 1,
+        scholarshipCategory : 1,
+        applicationFees: 1,
+        applicationDeadline: 1,
+        scholarshipLevel: 1,
+       });
+      const result = await cursor.toArray();
+      res.send(result);
+      
+    })
+
+    app.get('/top-scholarships', async (req, res) => {
+      const cursor = scholarshipsCollection.find()
+      .sort({scholarshipPostDate: -1})
+      .limit(6)
+      .project({ 
+        _id: 1,
+        scholarshipName: 1,
+        universityName: 1,
+        universityImage: 1,
+        universityCountry: 1,
+        universityCity: 1,
+        subjectCategory: 1,
+        scholarshipCategory : 1,
+        applicationFees: 1,
+        applicationDeadline: 1,
+        scholarshipLevel: 1,
+        
+       });
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+      
+
+    app.get('/scholarships/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await scholarshipsCollection.findOne(query);
+      res.send(result);
     })
 
 
