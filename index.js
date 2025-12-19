8a986c413bf0e7fc9f43a1cb6503cbdab194ff48
Rahat Murshed <rahatmurshedtapader@gmail.com>
2025-12-19 20:47:25 +0600

    Delete api problem fixed in scholarship collection

--- index.js
+++ index.js
@@ -164,6 +164,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/scholarship/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await scholarshipsCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // Payment Related Api
 
