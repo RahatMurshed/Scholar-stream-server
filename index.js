3ea7799d03b847b34d634ded7629eeb652a7625e
Rahat Murshed <rahatmurshedtapader@gmail.com>
2025-12-19 22:58:51 +0600

    users delete api added

--- index.js
+++ index.js
@@ -116,7 +116,14 @@ async function run() {
 
       const result = await usersCollection.updateOne(query, update);
       res.send(result);
-    })
+    });
+
+    app.delete('/user/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    });
 
 
 
