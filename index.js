4c79368cc11832d30ec6c9b7da43212459dd155e
Rahat Murshed <rahatmurshedtapader@gmail.com>
2026-01-12 19:15:42 +0600

    project polished

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ async function run() {
 
 
 
-    app.get('/scholarship/:id', verifyFirebaseToken, async (req, res) => {
+    app.get('/scholarship/:id',  async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await scholarshipsCollection.findOne(query);
@@ -513,8 +513,8 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   }
   finally {
     // Ensures that the client will close when you finish/error
